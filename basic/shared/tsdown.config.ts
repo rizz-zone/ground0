69568f647c0f85d2486f860320c43380f8886a22
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-07-30 12:01:59 +0100

    parse sql as text, export defs properly because we were doing a crazy path traversal before

--- basic/shared/tsdown.config.ts
+++ basic/shared/tsdown.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from 'tsdown'
+import { type PluginOption } from 'vite'
+
+const sqlRawPlugin: PluginOption = {
+	name: 'vite-plugin-sql-raw',
+	enforce: 'pre',
+	transform(code, id) {
+		if (id.endsWith('.sql')) {
+			return `export default \`${code.replaceAll('`', '\\`')}\`;`
+		}
+	}
+}
 
 export default defineConfig({
 	exports: true,
@@ -9,5 +20,6 @@ export default defineConfig({
 	sourcemap: true,
 	entry: {
 		index: 'src/index.ts'
-	}
+	},
+	plugins: [sqlRawPlugin]
 })
