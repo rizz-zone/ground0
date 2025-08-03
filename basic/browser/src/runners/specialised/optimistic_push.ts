c8c9d3ee78afd3e24607534ec2375e68af65787c
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-08-03 16:56:53 +0100

    combine all the runners in one file

---
+++ basic/browser/src/runners/specialised/optimistic_push.ts
@@ -0,0 +1,4 @@
+import type { TransitionImpact } from '@ground0/shared'
+import { TransitionRunner } from '../base'
+
+export class OptimisticPushTransitionRunner extends TransitionRunner<TransitionImpact.OptimisticPush> {}
