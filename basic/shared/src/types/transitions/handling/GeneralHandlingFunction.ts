b0adf89cf332ed53e9c50f90f01442c293b0ce18
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-08-07 19:08:43 +0100

    deprecate `GeneralHandlingFunction`

--- basic/shared/src/types/transitions/handling/GeneralHandlingFunction.ts
+++ basic/shared/src/types/transitions/handling/GeneralHandlingFunction.ts
@@ -1,6 +1,9 @@
 import type { Transition } from '../Transition'
 import type { HandlerParams } from './HandlerParams'
 
+/**
+ * @deprecated Turns out a 'general handler' is cringe 100% of the time
+ */
 export type GeneralHandlingFunction<T extends Transition> = (
 	params: HandlerParams<T>
 ) => unknown | Promise<unknown>
