1291e728aaac4397b8f65267d29427c6ed4fc98f
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-08-13 22:45:45 +0100

    make `optimistic_push.ts` not completely error out just by existing right now

--- basic/browser/src/runners/specialised/optimistic_push.ts
+++ basic/browser/src/runners/specialised/optimistic_push.ts
@@ -1,4 +1,16 @@
 import type { TransitionImpact } from '@ground0/shared'
 import { TransitionRunner } from '../base'
 
-export class OptimisticPushTransitionRunner extends TransitionRunner<TransitionImpact.OptimisticPush> {}
+export class OptimisticPushTransitionRunner<
+	MemoryModel extends object
+> extends TransitionRunner<MemoryModel, TransitionImpact.OptimisticPush> {
+	public override onDbConnected(): unknown {
+		throw new Error('Method not implemented.')
+	}
+	public override onDbConfirmedNeverConnecting(): unknown {
+		throw new Error('Method not implemented.')
+	}
+	public override onWsConnected(): unknown {
+		throw new Error('Method not implemented.')
+	}
+}
