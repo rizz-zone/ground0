6772127e5bb483f21f0e0f9fbf9e98e5a53871b1
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-08-27 14:52:16 +0100

    start actually properly working on the optimistic push state machine

--- basic/browser/src/machines/optimistic_push.ts
+++ basic/browser/src/machines/optimistic_push.ts
@@ -1,5 +1,88 @@
 import { setup } from 'xstate'
 
-export const optimisticPushMachine = setup({}).createMachine({
-	type: 'parallel'
+export const optimisticPushMachine = setup({
+	types: {
+		events: {} as
+			| { type: 'init' }
+			| { type: 'memory model completed' }
+			| { type: 'memory model failed' }
+			| { type: 'ws responded'; confirmed: boolean }
+	},
+	actions: {
+		editMemoryModel: () => {}
+	},
+	guards: {
+		memoryModelInProvidedHandler: ({ event }) =>
+			event.type === 'init' /* && event.something */,
+		wsResponseIsConfirmation: ({ event }) =>
+			event.type === 'ws responded' && event.confirmed
+	}
+}).createMachine({
+	type: 'parallel',
+	states: {
+		ws: {
+			initial: 'no response',
+			states: {
+				'no response': {
+					on: {
+						'ws responded': [
+							{
+								guard: 'wsResponseIsConfirmation',
+								target: 'confirmed'
+							},
+							{
+								target: 'rejected'
+							}
+						]
+					}
+				},
+				confirmed: {
+					type: 'final'
+				},
+				rejected: {
+					type: 'final'
+				}
+			}
+		},
+		'memory model': {
+			initial: 'not evaluated',
+			states: {
+				'not evaluated': {
+					on: {
+						init: [
+							{
+								guard: 'memoryModelInProvidedHandler',
+								target: 'in progress'
+							},
+							{
+								target: 'not required'
+							}
+						]
+					}
+				},
+				'in progress': {
+					entry: 'editMemoryModel',
+					on: {
+						'memory model failed': {
+							target: 'failed'
+						},
+						'memory model completed': {
+							target: 'completed'
+						}
+					}
+				},
+				failed: {
+					type: 'final'
+				},
+				completed: {},
+				reverting: {},
+				reverted: {
+					type: 'final'
+				},
+				'not required': {
+					type: 'final'
+				}
+			}
+		}
+	}
 })
