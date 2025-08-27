f8bfaa4dbb68a9a1cedb25042d7f38fc679abf2e
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-08-27 16:54:58 +0100

    probably complete the state graph

--- basic/browser/src/machines/optimistic_push.ts
+++ basic/browser/src/machines/optimistic_push.ts
@@ -1,45 +1,84 @@
-import { setup } from 'xstate'
+import type { LocalDatabase } from '@ground0/shared'
+import { and, assign, setup } from 'xstate'
 
 export const optimisticPushMachine = setup({
 	types: {
 		events: {} as
-			| { type: 'init' }
-			| { type: 'memory model completed' }
-			| { type: 'memory model failed' }
-			| { type: 'ws responded'; confirmed: boolean }
+			| { type: 'init' /* a bunch of other stuff */ }
+			| { type: 'memory model edit completed' }
+			| { type: 'memory model edit failed' }
+			| { type: 'memory model revert completed' }
+			| { type: 'memory model revert failed' }
+			| { type: 'db edit completed' }
+			| { type: 'db edit failed' }
+			| { type: 'db revert completed' }
+			| { type: 'db revert failed' }
+			| { type: 'db connected'; db: LocalDatabase }
+			| { type: 'db will not arrive' }
+			| { type: 'ws connected'; ws: WebSocket }
+			| { type: 'ws confirmed' }
+			| { type: 'ws rejected' }
 	},
 	actions: {
-		editMemoryModel: () => {}
+		editMemoryModel: () => {},
+		editDb: () => {},
+		revertMemoryModel: () => {},
+		revertDb: () => {},
+		setFinaliseFunction: assign(() => ({})),
+		setWs: assign(() => ({})),
+		setDb: assign(() => ({})),
+		setMemoryModel: assign(() => ({})),
+		setMemoryModelHandlerFunctions: assign(() => ({})),
+		setDbHandlerFunctions: assign(() => ({})),
+		logFailure: (_, responsibleHandlerArea: 'memory model' | 'db') => {
+			console.log(responsibleHandlerArea)
+		},
+		sendWsMessage: () => {},
+		finaliseIfApplicable: () => {}
 	},
 	guards: {
-		memoryModelInProvidedHandler: ({ event }) =>
+		memoryModelFunctionInProvidedHandler: ({ event }) =>
 			event.type === 'init' /* && event.something */,
-		wsResponseIsConfirmation: ({ event }) =>
-			event.type === 'ws responded' && event.confirmed
+		dbFunctionInProvidedHandler: ({ event }) =>
+			event.type === 'init' /* && event.somethingElse */,
+		dbConnectedAndProvided: ({ event }) =>
+			event.type === 'init' /* && event.somethingElse */,
+		dbWillNotArrive: ({ event }) =>
+			event.type === 'init' /* && event.somethingElse */
 	}
 }).createMachine({
 	type: 'parallel',
+	on: {
+		init: {
+			actions: ['setMemoryModel', 'setFinaliseFunction']
+		}
+	},
 	states: {
 		ws: {
 			initial: 'no response',
 			states: {
 				'no response': {
 					on: {
-						'ws responded': [
-							{
-								guard: 'wsResponseIsConfirmation',
-								target: 'confirmed'
-							},
-							{
-								target: 'rejected'
-							}
-						]
+						init: {
+							actions: ['setWs', 'sendWsMessage']
+						},
+						'ws connected': {
+							actions: ['setWs', 'sendWsMessage']
+						},
+						'ws confirmed': {
+							target: 'confirmed'
+						},
+						'ws rejected': {
+							target: 'rejected'
+						}
 					}
 				},
 				confirmed: {
+					entry: 'finaliseIfApplicable',
 					type: 'final'
 				},
 				rejected: {
+					entry: 'finaliseIfApplicable',
 					type: 'final'
 				}
 			}
@@ -51,8 +90,9 @@ export const optimisticPushMachine = setup({
 					on: {
 						init: [
 							{
-								guard: 'memoryModelInProvidedHandler',
-								target: 'in progress'
+								guard: 'memoryModelFunctionInProvidedHandler',
+								target: 'in progress',
+								actions: ['setMemoryModelHandlerFunctions']
 							},
 							{
 								target: 'not required'
@@ -63,23 +103,136 @@ export const optimisticPushMachine = setup({
 				'in progress': {
 					entry: 'editMemoryModel',
 					on: {
-						'memory model failed': {
+						'memory model edit failed': {
 							target: 'failed'
 						},
-						'memory model completed': {
+						'memory model edit completed': {
 							target: 'completed'
 						}
 					}
 				},
 				failed: {
+					entry: [
+						{ type: 'logFailure', params: 'memory model' },
+						'finaliseIfApplicable'
+					],
 					type: 'final'
 				},
-				completed: {},
-				reverting: {},
+				completed: {
+					entry: 'finaliseIfApplicable',
+					on: {
+						'ws rejected': {
+							target: 'reverting'
+						}
+					}
+				},
+				reverting: {
+					on: {
+						'memory model revert failed': {
+							target: 'failed'
+						},
+						'memory model revert completed': {
+							target: 'reverted'
+						}
+					}
+				},
 				reverted: {
+					entry: 'finaliseIfApplicable',
 					type: 'final'
 				},
 				'not required': {
+					type: 'final'
+				}
+			}
+		},
+		db: {
+			initial: 'not evaluated',
+			states: {
+				'not evaluated': {
+					on: {
+						init: [
+							{
+								guard: 'dbWillNotArrive',
+								target: 'not possible'
+							},
+							{
+								guard: and([
+									'dbFunctionInProvidedHandler',
+									'dbConnectedAndProvided'
+								]),
+								target: 'in progress',
+								actions: ['setDb', 'setDbHandlerFunctions']
+							},
+							{
+								guard: 'dbFunctionInProvidedHandler',
+								target: 'awaiting resources',
+								actions: ['setDbHandlerFunctions']
+							},
+							{
+								target: 'not required'
+							}
+						]
+					}
+				},
+				'awaiting resources': {
+					on: {
+						'db connected': {
+							target: 'in progress'
+						},
+						'db will not arrive': {
+							target: 'not possible'
+						},
+						'ws rejected': {
+							target: 'did not begin executing before rejection'
+						}
+					}
+				},
+				'in progress': {
+					entry: 'editDb',
+					on: {
+						'db edit failed': {
+							target: 'failed'
+						},
+						'db edit completed': {
+							target: 'completed'
+						}
+					}
+				},
+				completed: {
+					entry: 'finaliseIfApplicable',
+					on: {
+						'ws rejected': {
+							target: 'reverting'
+						}
+					}
+				},
+				failed: {
+					entry: [{ type: 'logFailure', params: 'db' }, 'finaliseIfApplicable'],
+					type: 'final'
+				},
+				reverting: {
+					entry: 'revertDb',
+					on: {
+						'db revert failed': {
+							target: 'failed'
+						},
+						'db revert completed': {
+							target: 'reverted'
+						}
+					}
+				},
+				reverted: {
+					entry: 'finaliseIfApplicable',
+					type: 'final'
+				},
+				'did not begin executing before rejection': {
+					type: 'final'
+				},
+				'not required': {
+					type: 'final'
+				},
+				'not possible': {
+					entry: 'finaliseIfApplicable',
 					type: 'final'
 				}
 			}
