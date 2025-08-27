9527d93ebc2024981c24cd0c4804fbe85d34eb34
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-08-27 23:05:45 +0100

    deprecate `ResourceStatus` because it turns out it's pretty cringe

--- basic/browser/src/types/status/ResourceStatus.ts
+++ basic/browser/src/types/status/ResourceStatus.ts
@@ -1,6 +1,10 @@
 import type { DbResourceStatus } from './DbResourceStatus'
 import type { WsResourceStatus } from './WsResourceStatus'
 
+/**
+ * @deprecated A `ResourceBundle` is preferable in new code because it connects
+ * a status with actual available resources, which TypeScript is more fond of.
+ */
 export type ResourceStatus = {
 	db: DbResourceStatus
 	ws: WsResourceStatus
