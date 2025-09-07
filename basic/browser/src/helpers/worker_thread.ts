a1adfbf172edc1ba880d160c90f1d8dafeb3770e
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-07 18:31:56 +0100

    work on the ws side of `WorkerLocalFirst`. hopefully this will handle things decently

--- basic/browser/src/helpers/worker_thread.ts
+++ basic/browser/src/helpers/worker_thread.ts
@@ -1,15 +1,20 @@
 /// <reference lib="webworker" />
 
-import type {
-	LocalHandlers,
-	SyncEngineDefinition,
-	Transition
+import {
+	DownstreamWsMessageAction,
+	UpstreamWsMessageAction,
+	type DownstreamWsMessage,
+	type LocalHandlers,
+	type SyncEngineDefinition,
+	type Transition,
+	type UpstreamWsMessage
 } from '@ground0/shared'
 import type { Transformation } from '@/types/memory_model/Tranformation'
 import type { ResourceBundle } from '@/types/status/ResourceBundle'
 import { WsResourceStatus } from '@/types/status/WsResourceStatus'
 import { DbResourceStatus } from '@/types/status/DbResourceStatus'
 import { createMemoryModel } from './memory_model'
+import SuperJSON from 'superjson'
 
 export class WorkerLocalFirst<
 	MemoryModel extends object,
@@ -59,14 +64,69 @@ export class WorkerLocalFirst<
 		if (shared) this.connectDb()
 		this.connectWs()
 	}
+	public transition() {}
 
 	private syncResources(modifications: Partial<ResourceBundle>) {
 		if (modifications.db) {
 			this.resourceBundle.db = modifications.db
+			// TODO: Loop through anything that depends on the db
+		}
+		if (modifications.ws) {
+			this.resourceBundle.ws = modifications.ws
+			// TODO: Loop through anything that depends on the ws
 		}
 	}
+
+	// TODO: actual like db value and stuff
 	private async connectDb() {}
-	private async connectWs() {}
+
+	private ws?: WebSocket
+	private async connectWs() {
+		const ws = new WebSocket(this.wsUrl)
+		this.ws = ws
+		ws.onopen = () => {
+			if (this.ws !== ws) {
+				ws.close()
+				return
+			}
+			ws.send(
+				SuperJSON.stringify({
+					action: UpstreamWsMessageAction.Init,
+					version: this.engineDef.version.current
+				} satisfies UpstreamWsMessage)
+			)
+			this.syncResources({
+				ws: { status: WsResourceStatus.Connected, instance: ws }
+			})
+		}
+		// TODO: Complete these
+		ws.onmessage = (message) => {
+			let decoded: DownstreamWsMessage
+			try {
+				decoded = SuperJSON.parse(message.data)
+				if (!('action' in decoded)) throw new Error()
+			} catch {
+				// TODO: Handle this
+				return
+			}
+			switch (decoded.action) {
+				case DownstreamWsMessageAction.OptimisticCancel:
+				case DownstreamWsMessageAction.OptimisticResolve: {
+					return
+				}
+				default:
+					return
+			}
+		}
+		ws.onerror = () => {
+			ws.close()
+			if (this.ws === ws) this.connectWs()
+		}
+		ws.onclose = () => {
+			if (this.ws !== ws) return
+			this.connectWs()
+		}
+	}
 
 	public [Symbol.dispose] = () => {
 		// TODO: Put something here if it feels particularly relevant. But it doesn't really
