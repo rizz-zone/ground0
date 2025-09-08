f1966405e10dc15b20744c930c51c5cd9c0235c7
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-08 23:50:54 +0100

    allow for slightly nicer logs in the browser, so it's more obvious when something comes from ground0 (relevant in next commit)

---
+++ basic/browser/src/common/branded_log.ts
@@ -0,0 +1,17 @@
+/**
+ * Log with a **`[ground0] `** prefix to ensure the source is clear.
+ * @param functionToUse The logging function to use, like `console.debug` or `console.warn`.
+ * @param args The arguments you would pass to that function.
+ */
+export function brandedLog(
+	functionToUse:
+		| typeof console.debug
+		| typeof console.log
+		| typeof console.warn
+		| typeof console.error
+		| typeof console.info
+		| typeof console.trace,
+	...args: Parameters<typeof console.debug>
+) {
+	functionToUse('%c[ground0]', 'font-weight:bold', ...args)
+}
