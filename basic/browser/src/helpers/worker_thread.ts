f556fabe9a29ac695ee1b05db5ee9771dd00dd90
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-08 23:53:29 +0100

    (probably?) complete the functionality of the actual sqlite setup code in `WorkerLocalFirst`. it doesn't set drizzle up yet, but this is next. also it'll probably be worth splitting this logic out soon because we currently have a 308 line file. at least we can, unlike with the machine from before that didn't even have db code and was already bigger than this and was kind of naturally impossible to split

--- basic/browser/src/helpers/worker_thread.ts
+++ basic/browser/src/helpers/worker_thread.ts
@@ -26,6 +26,7 @@ import { OPFSCoopSyncVFS } from 'wa-sqlite/src/examples/OPFSCoopSyncVFS.js'
 import SQLiteESMFactory from 'wa-sqlite/dist/wa-sqlite.mjs'
 // @ts-expect-error wa-sqlite has no type definitions
 import * as SQLite from 'wa-sqlite/src/sqlite-api.js'
+import { brandedLog } from '@/common/branded_log'
 
 export class WorkerLocalFirst<
 	MemoryModel extends object,
@@ -89,7 +90,6 @@ export class WorkerLocalFirst<
 		}
 	}
 
-	// TODO: Finish connectDb, which will be a bit painful because sqlite
 	private async connectDb(pullWasmBinary: () => Promise<ArrayBuffer>) {
 		// connectDb shouldn't be called if the db will never connect, but it's
 		// worth checking anyway
@@ -124,13 +124,57 @@ export class WorkerLocalFirst<
 		const sqlite3 = SQLite.Factory(module)
 
 		// Register a custom file system.
-		// TODO: Figure out if we can / should just be calling it 'hello' or if
-		// that's bad and should cnange
-		const vfs = await OPFSCoopSyncVFS.create('hello', module)
+		const vfs = await OPFSCoopSyncVFS.create('opfs', module)
 		sqlite3.vfs_register(vfs, true)
 
 		// Open the database.
 		const db = await sqlite3.open_v2(this.dbName) // NOTE TO SELF: THIS IS A POINTER
+
+		// TODO: Use our own errors instead of these ones for Clarity
+
+		let pageSize: number | undefined
+		if (
+			(await sqlite3.exec(db, `PRAGMA page_size;`, (row: number[]) => {
+				pageSize = row[0]
+			})) !== SQLite.SQLITE_OK
+		)
+			throw new Error('Could not get page size')
+		let pageCount: number | undefined
+		if (
+			(await sqlite3.exec(db, `PRAGMA page_count;`, (row: number[]) => {
+				pageCount = row[0]
+			})) !== SQLite.SQLITE_OK
+		)
+			throw new Error('Could not get page count')
+		if (typeof pageSize === 'undefined' || typeof pageCount === 'undefined')
+			throw new Error('SQLite is not reporting storage')
+
+		const { quota } = await navigator.storage.estimate()
+		if (!quota) throw new Error('Browser is not reporting storage quota')
+		brandedLog(
+			console.debug,
+			`${this.dbName} is using ${pageSize * pageCount}B (${quota}B available - ${Math.floor((pageSize * pageCount) / quota)}% used)`
+		)
+
+		// Set a page limit so that the database doesn't exceed available
+		// quota. We'll use 90% of the available quota as a safety margin.
+		const maxBytes = Math.floor(quota * 0.9)
+		const maxPages = Math.floor(maxBytes / pageSize)
+		const setPageLimitResult = await sqlite3.exec(
+			db,
+			`PRAGMA max_page_count = ${maxPages};`
+		)
+		if (setPageLimitResult !== SQLite.SQLITE_OK) {
+			throw new Error('Could not set max_page_count')
+		}
+		brandedLog(
+			console.debug,
+			`Set max_page_count to ${maxPages} (~${maxBytes}B)`
+		)
+
+		// TODO: Turn this into a drizzle db. This code from the cloneathon
+		// will come in handy:
+		// https://github.com/ezShroom/rizz-chat/blob/17f6fc962e65642c6a04e8c273be0f16cf99474d/packages/sveltekit/src/lib/sync_layer/workers/backend/storage/index.ts#L135-L163
 	}
 
 	private dissatisfiedPings = 0
