0e33b192be900de830e37ffdd11fc3a2f57f257a
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-09 22:20:04 +0100

    use the new thing in `WorkerLocalFirst`

--- basic/browser/src/helpers/worker_thread.ts
+++ basic/browser/src/helpers/worker_thread.ts
@@ -20,12 +20,8 @@ import SuperJSON from 'superjson'
 import type { TransitionRunner } from '@/runners/base'
 import { runners } from '@/runners/all'
 import type { OptimisticPushTransitionRunner } from '@/runners/specialised/optimistic_push'
-// @ts-expect-error wa-sqlite has no type definitions
-import { OPFSCoopSyncVFS } from 'wa-sqlite/src/examples/OPFSCoopSyncVFS.js'
-import SQLiteESMFactory from 'wa-sqlite/dist/wa-sqlite.mjs'
-// @ts-expect-error wa-sqlite has no type definitions
-import * as SQLite from 'wa-sqlite/src/sqlite-api.js'
-import { brandedLog } from '@/common/branded_log'
+import { connectDb } from '@/resource_managers/db'
+import type { Migrations } from '@/types/Migrations'
 
 export class WorkerLocalFirst<
 	MemoryModel extends object,
@@ -33,7 +29,6 @@ export class WorkerLocalFirst<
 > {
 	private readonly resourceBundle: ResourceBundle
 	private readonly wsUrl: string
-	private readonly dbName: string
 	private readonly engineDef: SyncEngineDefinition<TransitionSchema>
 	private readonly localHandlers: LocalHandlers<MemoryModel, TransitionSchema>
 	private readonly memoryModel: MemoryModel
@@ -45,7 +40,8 @@ export class WorkerLocalFirst<
 		localHandlers,
 		initialMemoryModel,
 		announceTransformation,
-		pullWasmBinary
+		pullWasmBinary,
+		migrations
 	}: {
 		wsUrl: string
 		dbName: string
@@ -53,7 +49,8 @@ export class WorkerLocalFirst<
 		localHandlers: LocalHandlers<MemoryModel, TransitionSchema>
 		initialMemoryModel: MemoryModel
 		announceTransformation: (transformation: Transformation) => unknown
-		pullWasmBinary: () => Promise<ArrayBuffer> // TODO: use a proper type that actually helps. also set this
+		pullWasmBinary: () => Promise<ArrayBuffer>
+		migrations: Migrations
 	}) {
 		const shared = 'onconnect' in self
 		this.resourceBundle = {
@@ -66,7 +63,6 @@ export class WorkerLocalFirst<
 		}
 
 		this.wsUrl = wsUrl
-		this.dbName = dbName
 		this.engineDef = engineDef
 		this.localHandlers = localHandlers
 		this.memoryModel = createMemoryModel(
@@ -74,7 +70,13 @@ export class WorkerLocalFirst<
 			announceTransformation
 		)
 
-		if (shared) this.connectDb(pullWasmBinary)
+		if (shared)
+			connectDb({
+				pullWasmBinary,
+				syncResources: this.syncResources.bind(this),
+				dbName,
+				migrations
+			})
 		this.connectWs()
 	}
 
@@ -87,93 +89,6 @@ export class WorkerLocalFirst<
 			this.resourceBundle.ws = modifications.ws
 			// TODO: Loop through anything that depends on the ws
 		}
-	}
-
-	private async connectDb(pullWasmBinary: () => Promise<ArrayBuffer>) {
-		// connectDb shouldn't be called if the db will never connect, but it's
-		// worth checking anyway
-		// TODO: Make this error message more Detailed
-		if (this.resourceBundle.db.status !== DbResourceStatus.Disconnected)
-			return console.warn(
-				'there is a db, or the db is not connecting, why has connectDb been called'
-			)
-
-		// Get the wasm with the code of the adapter. It's the adapter's
-		// responsibility to do this, including providing a retry method
-		const module = await pullWasmBinary().then(
-			(wasm) =>
-				SQLiteESMFactory({
-					instantiateWasm: (
-						imports: WebAssembly.Imports,
-						successCallback: (instance: WebAssembly.Instance) => void
-					) => {
-						WebAssembly.instantiate(wasm, imports).then(({ instance }) => {
-							successCallback(instance)
-						})
-						return {} // emscripten requires this return
-					}
-				}),
-			() => {
-				this.syncResources({ db: { status: DbResourceStatus.NeverConnecting } })
-			}
-		)
-		// The module will be undefined if onrejected was called
-		if (typeof module === 'undefined') return
-
-		const sqlite3 = SQLite.Factory(module)
-
-		// Register a custom file system.
-		const vfs = await OPFSCoopSyncVFS.create('opfs', module)
-		sqlite3.vfs_register(vfs, true)
-
-		// Open the database.
-		const db = await sqlite3.open_v2(this.dbName) // NOTE TO SELF: THIS IS A POINTER
-
-		// TODO: Use our own errors instead of these ones for Clarity
-
-		let pageSize: number | undefined
-		if (
-			(await sqlite3.exec(db, `PRAGMA page_size;`, (row: number[]) => {
-				pageSize = row[0]
-			})) !== SQLite.SQLITE_OK
-		)
-			throw new Error('Could not get page size')
-		let pageCount: number | undefined
-		if (
-			(await sqlite3.exec(db, `PRAGMA page_count;`, (row: number[]) => {
-				pageCount = row[0]
-			})) !== SQLite.SQLITE_OK
-		)
-			throw new Error('Could not get page count')
-		if (typeof pageSize === 'undefined' || typeof pageCount === 'undefined')
-			throw new Error('SQLite is not reporting storage')
-
-		const { quota } = await navigator.storage.estimate()
-		if (!quota) throw new Error('Browser is not reporting storage quota')
-		brandedLog(
-			console.debug,
-			`${this.dbName} is using ${pageSize * pageCount}B (${quota}B available - ${Math.floor((pageSize * pageCount) / quota)}% used)`
-		)
-
-		// Set a page limit so that the database doesn't exceed available
-		// quota. We'll use 90% of the available quota as a safety margin.
-		const maxBytes = Math.floor(quota * 0.9)
-		const maxPages = Math.floor(maxBytes / pageSize)
-		const setPageLimitResult = await sqlite3.exec(
-			db,
-			`PRAGMA max_page_count = ${maxPages};`
-		)
-		if (setPageLimitResult !== SQLite.SQLITE_OK) {
-			throw new Error('Could not set max_page_count')
-		}
-		brandedLog(
-			console.debug,
-			`Set max_page_count to ${maxPages} (~${maxBytes}B)`
-		)
-
-		// TODO: Turn this into a drizzle db. This code from the cloneathon
-		// will come in handy:
-		// https://github.com/ezShroom/rizz-chat/blob/17f6fc962e65642c6a04e8c273be0f16cf99474d/packages/sveltekit/src/lib/sync_layer/workers/backend/storage/index.ts#L135-L163
 	}
 
 	private dissatisfiedPings = 0
