d2eef74782d119096c999d31821e085fec73e071
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-09 12:19:51 +0100

    remove the class wrapper entirely, because it's unnecessary when `WorkerLocalFirst` just expects a `syncResources` call back, *once*

--- basic/browser/src/resource_managers/db/index.ts
+++ basic/browser/src/resource_managers/db/index.ts
@@ -8,96 +8,86 @@ import type { ResourceBundle } from '@/types/status/ResourceBundle'
 import { DbResourceStatus } from '@/types/status/DbResourceStatus'
 import { brandedLog } from '@/common/branded_log'
 
-export class DbResourceManager {
-	private syncResources
-	private dbName
-	public constructor({
-		syncResources,
-		dbName
-	}: {
-		syncResources: (modifications: Partial<ResourceBundle>) => unknown
-		dbName: string
-	}) {
-		this.syncResources = syncResources
-		this.dbName = dbName
-	}
-
-	private async connectDb(pullWasmBinary: () => Promise<ArrayBuffer>) {
-		// Get the wasm with the code of the adapter. It's the adapter's
-		// responsibility to do this, including providing a retry method
-		const module = await pullWasmBinary().then(
-			(wasm) =>
-				SQLiteESMFactory({
-					instantiateWasm: (
-						imports: WebAssembly.Imports,
-						successCallback: (instance: WebAssembly.Instance) => void
-					) => {
-						WebAssembly.instantiate(wasm, imports).then(({ instance }) => {
-							successCallback(instance)
-						})
-						return {} // emscripten requires this return
-					}
-				}),
-			() => {
-				this.syncResources({ db: { status: DbResourceStatus.NeverConnecting } })
-			}
-		)
-		// The module will be undefined if onrejected was called
-		if (typeof module === 'undefined') return
-
-		const sqlite3 = SQLite.Factory(module)
-
-		// Register a custom file system.
-		const vfs = await OPFSCoopSyncVFS.create('opfs', module)
-		sqlite3.vfs_register(vfs, true)
-
-		// Open the database.
-		const db = await sqlite3.open_v2(this.dbName) // NOTE TO SELF: THIS IS A POINTER
-
-		// TODO: Use our own errors instead of these ones for Clarity
-
-		let pageSize: number | undefined
-		if (
-			(await sqlite3.exec(db, `PRAGMA page_size;`, (row: number[]) => {
-				pageSize = row[0]
-			})) !== SQLite.SQLITE_OK
-		)
-			throw new Error('Could not get page size')
-		let pageCount: number | undefined
-		if (
-			(await sqlite3.exec(db, `PRAGMA page_count;`, (row: number[]) => {
-				pageCount = row[0]
-			})) !== SQLite.SQLITE_OK
-		)
-			throw new Error('Could not get page count')
-		if (typeof pageSize === 'undefined' || typeof pageCount === 'undefined')
-			throw new Error('SQLite is not reporting storage')
-
-		const { quota } = await navigator.storage.estimate()
-		if (!quota) throw new Error('Browser is not reporting storage quota')
-		brandedLog(
-			console.debug,
-			`${this.dbName} is using ${pageSize * pageCount}B (${quota}B available - ${Math.floor((pageSize * pageCount) / quota)}% used)`
-		)
-
-		// Set a page limit so that the database doesn't exceed available
-		// quota. We'll use 90% of the available quota as a safety margin.
-		const maxBytes = Math.floor(quota * 0.9)
-		const maxPages = Math.floor(maxBytes / pageSize)
-		const setPageLimitResult = await sqlite3.exec(
-			db,
-			`PRAGMA max_page_count = ${maxPages};`
-		)
-		if (setPageLimitResult !== SQLite.SQLITE_OK) {
-			throw new Error('Could not set max_page_count')
+export async function connectDb({
+	syncResources,
+	dbName,
+	pullWasmBinary
+}: {
+	syncResources: (modifications: Partial<ResourceBundle>) => unknown
+	pullWasmBinary: () => Promise<ArrayBuffer>
+	dbName: string
+}) {
+	// Get the wasm with the code of the adapter. It's the adapter's
+	// responsibility to do this, including providing a retry method
+	const module = await pullWasmBinary().then(
+		(wasm) =>
+			SQLiteESMFactory({
+				instantiateWasm: (
+					imports: WebAssembly.Imports,
+					successCallback: (instance: WebAssembly.Instance) => void
+				) => {
+					WebAssembly.instantiate(wasm, imports).then(({ instance }) => {
+						successCallback(instance)
+					})
+					return {} // emscripten requires this return
+				}
+			}),
+		() => {
+			syncResources({ db: { status: DbResourceStatus.NeverConnecting } })
 		}
-		brandedLog(
-			console.debug,
-			`Set max_page_count to ${maxPages} (~${maxBytes}B)`
-		)
+	)
+	// The module will be undefined if onrejected was called
+	if (typeof module === 'undefined') return
 
-		// TODO: Turn this into a drizzle db. This code from the cloneathon
-		// will come in handy:
-		// https://github.com/ezShroom/rizz-chat/blob/17f6fc962e65642c6a04e8c273be0f16cf99474d/packages/sveltekit/src/lib/sync_layer/workers/backend/storage/index.ts#L135-L163
+	const sqlite3 = SQLite.Factory(module)
+
+	// Register a custom file system.
+	const vfs = await OPFSCoopSyncVFS.create('opfs', module)
+	sqlite3.vfs_register(vfs, true)
+
+	// Open the database.
+	const db = await sqlite3.open_v2(dbName) // NOTE TO SELF: THIS IS A POINTER
+
+	// TODO: Use our own errors instead of these ones for Clarity
+
+	let pageSize: number | undefined
+	if (
+		(await sqlite3.exec(db, `PRAGMA page_size;`, (row: number[]) => {
+			pageSize = row[0]
+		})) !== SQLite.SQLITE_OK
+	)
+		throw new Error('Could not get page size')
+	let pageCount: number | undefined
+	if (
+		(await sqlite3.exec(db, `PRAGMA page_count;`, (row: number[]) => {
+			pageCount = row[0]
+		})) !== SQLite.SQLITE_OK
+	)
+		throw new Error('Could not get page count')
+	if (typeof pageSize === 'undefined' || typeof pageCount === 'undefined')
+		throw new Error('SQLite is not reporting storage')
+
+	const { quota } = await navigator.storage.estimate()
+	if (!quota) throw new Error('Browser is not reporting storage quota')
+	brandedLog(
+		console.debug,
+		`${dbName} is using ${pageSize * pageCount}B (${quota}B available - ${Math.floor((pageSize * pageCount) / quota)}% used)`
+	)
+
+	// Set a page limit so that the database doesn't exceed available
+	// quota. We'll use 90% of the available quota as a safety margin.
+	const maxBytes = Math.floor(quota * 0.9)
+	const maxPages = Math.floor(maxBytes / pageSize)
+	const setPageLimitResult = await sqlite3.exec(
+		db,
+		`PRAGMA max_page_count = ${maxPages};`
+	)
+	if (setPageLimitResult !== SQLite.SQLITE_OK) {
+		throw new Error('Could not set max_page_count')
 	}
+	brandedLog(console.debug, `Set max_page_count to ${maxPages} (~${maxBytes}B)`)
+
+	// TODO: Turn this into a drizzle db. This code from the cloneathon
+	// will come in handy:
+	// https://github.com/ezShroom/rizz-chat/blob/17f6fc962e65642c6a04e8c273be0f16cf99474d/packages/sveltekit/src/lib/sync_layer/workers/backend/storage/index.ts#L135-L163
 }
