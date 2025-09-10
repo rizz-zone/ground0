fdb480e1f6b1fff6f90fc4cf2a5135e79625d3df
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-10 22:50:35 +0100

    create a `WorkerLocalFirst` instance on the new general `workerEntrypoint` as well as allowing message broadcasts (used for transitions, soon also for initial memory model state). Also start parsing messages coming in, and handle messaging errors

--- basic/browser/src/exports/entrypoints/general.ts
+++ basic/browser/src/exports/entrypoints/general.ts
@@ -1,24 +1,72 @@
+import { brandedLog } from '@/common/branded_log'
+import { WorkerLocalFirst } from '@/helpers/worker_thread'
+import type { LocalEngineDefinition } from '@/types/LocalEngineDefinition'
+import {
+	DownstreamWorkerMessageType,
+	type DownstreamWorkerMessage
+} from '@/types/internal_messages/DownstreamWorkerMessage'
 import {
 	UpstreamWorkerMessageType,
 	type UpstreamWorkerMessage
 } from '@/types/internal_messages/UpstreamWorkerMessage'
-import {
-	NoPortsError,
-	type Transition,
-	type TransitionSchema
-} from '@ground0/shared'
+import { NoPortsError, type Transition } from '@ground0/shared'
 
 const ctx = self as unknown as
 	| SharedWorkerGlobalScope
 	| DedicatedWorkerGlobalScope
 
-export function workerEntrypoint<T extends Transition>() {
+export function workerEntrypoint<
+	MemoryModel extends object,
+	T extends Transition
+>({
+	engineDef,
+	localHandlers,
+	initialMemoryModel,
+	migrations,
+	pullWasmBinary,
+	wsUrl,
+	dbName
+}: LocalEngineDefinition<MemoryModel, T>) {
 	const ports: MessagePort[] = []
 
+	function broadcastMessage(message: DownstreamWorkerMessage) {
+		if ('onconnect' in ctx) for (const port of ports) port.postMessage(message)
+		else ctx.postMessage(message)
+	}
+
 	// Establish a WorkerLocalFirst
-	// TODO: Fill this part out
+	const workerLocalFirst = new WorkerLocalFirst({
+		wsUrl,
+		dbName,
+		engineDef,
+		localHandlers,
+		initialMemoryModel,
+		announceTransformation: (transformation) =>
+			broadcastMessage({
+				type: DownstreamWorkerMessageType.Transformation,
+				transformation
+			}),
+		pullWasmBinary,
+		migrations
+	})
+
+	function onmessage(event: MessageEvent<UpstreamWorkerMessage<T>>) {
+		const message = event.data
+		switch (message.type) {
+			case UpstreamWorkerMessageType.Transition:
+				workerLocalFirst.transition(message.data)
+				break
+		}
+	}
+	function onmessageerror() {
+		brandedLog(
+			console.error,
+			'There was a message error while receiving an upstream message!'
+		)
+	}
 
 	// Set listeners
+	// TODO: Send the state of the memory model on connect
 	if ('onconnect' in ctx) {
 		ctx.onconnect = (event) => {
 			const port = event.ports[0]
@@ -29,17 +77,11 @@ export function workerEntrypoint<T extends Transition>() {
 				)
 
 			ports.push(port)
+			port.onmessage = onmessage
+			port.onmessageerror = onmessageerror
 		}
 	} else {
-		self.onmessage = (
-			event: MessageEvent<UpstreamWorkerMessage<TransitionSchema<T>>>
-		) => {
-			const message = event.data
-			switch (message.type) {
-				case UpstreamWorkerMessageType.Transition: {
-					// TODO: Tell the WorkerLocalFirst about the transition
-				}
-			}
-		}
+		ctx.onmessage = onmessage
+		ctx.onmessageerror = onmessageerror
 	}
 }
