421a3b8d4a3755a0b5310b587b42cb4f87b1347d
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-15 14:42:05 +0100

    start making a proper test file with mocks and stuff

--- basic/browser/src/exports/entrypoints/general.test.ts
+++ basic/browser/src/exports/entrypoints/general.test.ts
@@ -1,5 +1,22 @@
 // TODO: Put some tests in here
 
-import { test } from 'vitest'
+import { describe, test, vi } from 'vitest'
+import { workerEntrypoint } from './general'
 
-test('no tests so far', () => {})
+const sharedCtx = self as unknown as SharedWorkerGlobalScope
+const dedicatedCtx = self as DedicatedWorkerGlobalScope
+
+const WorkerLocalFirst = vi.fn()
+const workerThreadHelperMock = vi.mock('../helpers/worker_thread', () => {
+	return {
+		WorkerLocalFirst
+	}
+})
+
+const minimumInput: Parameters<typeof workerEntrypoint>[0] = {}
+
+describe('always', () => {
+	test('creates a WorkerLocalFirst', () => {
+		workerEntrypoint()
+	})
+})
