4320b0227585f02ace1d7c51de877a6fec35f6de
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-09-16 12:11:55 +0100

    start making the `minimumInput`

--- basic/browser/src/exports/entrypoints/general.test.ts
+++ basic/browser/src/exports/entrypoints/general.test.ts
@@ -2,6 +2,9 @@
 
 import { describe, test, vi } from 'vitest'
 import { workerEntrypoint } from './general'
+import { object, literal, type infer } from 'zod'
+import { createTransitionSchema, TransitionImpact } from '@ground0/shared'
+import type { GeneratedMigrationSchema } from '../../../../shared/dist/types/transitions/handling/GeneratedMigrationSchema'
 
 const sharedCtx = self as unknown as SharedWorkerGlobalScope
 const dedicatedCtx = self as DedicatedWorkerGlobalScope
@@ -13,7 +16,35 @@ const workerThreadHelperMock = vi.mock('../helpers/worker_thread', () => {
 	}
 })
 
-const minimumInput: Parameters<typeof workerEntrypoint>[0] = {}
+const OurTransitionSchema = object({
+	action: literal('abc'),
+	impact: literal(TransitionImpact.LocalOnly)
+})
+
+const minimumInput: Parameters<typeof workerEntrypoint<Record<unknown, never>, infer<typeof OurTransitionSchema>>[0] = {
+	engineDef: {
+		transitions: {
+			schema: createTransitionSchema(OurTransitionSchema),
+			sharedHandlers: {}
+		},
+		version: {
+			current: '1.2.3'
+		},
+		db: {
+			migrations: {} as GeneratedMigrationSchema
+		}
+	},
+    localHandlers: {
+		'abc': {
+
+		}
+	},
+    initialMemoryModel: {},
+    migrations: Migrations;
+    pullWasmBinary: () => Promise<ArrayBuffer>;
+    wsUrl: string;
+    dbName: string;
+}
 
 describe('always', () => {
 	test('creates a WorkerLocalFirst', () => {
