3257bb65da2314485db3270dee677ba9b5dfc756
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-10-17 09:06:06 +0100

    fresh new browser tests

--- basic/browser/src/exports/browser.test.ts
+++ basic/browser/src/exports/browser.test.ts
@@ -1,128 +1,89 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { BrowserLocalFirst } from './browser'
-import { DB_NAME, SOCKET_URL } from '@/testing/constants'
 import {
 	type UpstreamWorkerMessage,
 	UpstreamWorkerMessageType
 } from '@/types/internal_messages/UpstreamWorkerMessage'
 import { type TestingTransition, TransitionImpact } from '@ground0/shared'
 
-const setIntervalMock = vi
-	.spyOn(globalThis, 'setInterval')
-	.mockImplementation(() => 1 as unknown as NodeJS.Timeout)
-
-describe('BrowserLocalFirst', () => {
-	beforeEach(() => setIntervalMock.mockClear())
-	describe('Worker', () => {
-		describe('message posting via .postMessage()', () => {
-			let mockWorker: Worker
-			beforeEach(() => {
-				mockWorker = {
-					postMessage: vi.fn()
-				} as unknown as Worker
-			})
-
-			it('inits', () => {
-				new BrowserLocalFirst({
-					dbName: DB_NAME,
-					wsUrl: SOCKET_URL,
-					worker: mockWorker
-				})
-
-				expect(mockWorker.postMessage).toHaveBeenCalledExactlyOnceWith({
-					type: UpstreamWorkerMessageType.Init,
-					data: {
-						dbName: DB_NAME,
-						wsUrl: SOCKET_URL
-					}
-				} satisfies UpstreamWorkerMessage<TestingTransition>)
-			})
-			it('sends transitions', () => {
-				const syncEngine = new BrowserLocalFirst<TestingTransition>({
-					dbName: DB_NAME,
-					wsUrl: SOCKET_URL,
-					worker: mockWorker
-				})
-				syncEngine.transition({
-					action: 'shift_foo_bar',
-					impact: TransitionImpact.LocalOnly
-				})
-
-				expect(mockWorker.postMessage).toHaveBeenLastCalledWith({
-					type: UpstreamWorkerMessageType.Transition,
-					data: {
-						action: 'shift_foo_bar',
-						impact: TransitionImpact.LocalOnly
-					}
-				})
-			})
-			it('does not set a timer', () => {
-				new BrowserLocalFirst<TestingTransition>({
-					dbName: DB_NAME,
-					wsUrl: SOCKET_URL,
-					worker: mockWorker
-				})
-				expect(setIntervalMock).not.toHaveBeenCalled()
-			})
-		})
-	})
-	describe('SharedWorker', () => {
-		type TestingSharedWorker = SharedWorker & {
-			postMessage: Worker['postMessage']
-		}
-		let mockWorker: TestingSharedWorker
+describe('Worker', () => {
+	describe('message posting via .postMessage()', () => {
+		let mockWorker: Worker
 		beforeEach(() => {
 			mockWorker = {
-				port: { postMessage: vi.fn() },
 				postMessage: vi.fn()
-			} as unknown as TestingSharedWorker
+			} as unknown as Worker
 		})
 
-		it('sets a timer', () => {
-			new BrowserLocalFirst<TestingTransition>({
-				dbName: DB_NAME,
-				wsUrl: SOCKET_URL,
-				worker: mockWorker
-			})
-			expect(setIntervalMock).toHaveBeenCalledOnce()
+		it('does not send any message on construction', () => {
+			new BrowserLocalFirst(mockWorker)
+			expect(mockWorker.postMessage).not.toHaveBeenCalled()
 		})
-		describe('message posting via .port.postMessage()', () => {
-			it('inits', () => {
-				new BrowserLocalFirst({
-					dbName: DB_NAME,
-					wsUrl: SOCKET_URL,
-					worker: mockWorker
-				})
-
-				expect(mockWorker.port.postMessage).toHaveBeenCalledExactlyOnceWith({
-					type: UpstreamWorkerMessageType.Init,
-					data: {
-						dbName: DB_NAME,
-						wsUrl: SOCKET_URL
-					}
-				} satisfies UpstreamWorkerMessage<TestingTransition>)
-				expect(mockWorker.postMessage).not.toBeCalled()
+		it('sends transitions', () => {
+			const syncEngine = new BrowserLocalFirst<TestingTransition>(mockWorker)
+			syncEngine.transition({
+				action: 'shift_foo_bar',
+				impact: TransitionImpact.LocalOnly
 			})
-			it('sends transitions', () => {
-				const syncEngine = new BrowserLocalFirst<TestingTransition>({
-					dbName: DB_NAME,
-					wsUrl: SOCKET_URL,
-					worker: mockWorker
-				})
-				syncEngine.transition({
+
+			expect(mockWorker.postMessage).toHaveBeenLastCalledWith({
+				type: UpstreamWorkerMessageType.Transition,
+				data: {
 					action: 'shift_foo_bar',
 					impact: TransitionImpact.LocalOnly
-				})
+				}
+			} satisfies UpstreamWorkerMessage<TestingTransition>)
+		})
+		it('sends Close on dispose', () => {
+			const syncEngine = new BrowserLocalFirst<TestingTransition>(mockWorker)
+			syncEngine[Symbol.dispose]()
+			expect(mockWorker.postMessage).toHaveBeenLastCalledWith({
+				type: UpstreamWorkerMessageType.Close
+			} satisfies UpstreamWorkerMessage<TestingTransition>)
+		})
+	})
+})
+describe('SharedWorker', () => {
+	type TestingSharedWorker = SharedWorker & {
+		postMessage: Worker['postMessage']
+	}
+	let mockWorker: TestingSharedWorker
+	beforeEach(() => {
+		mockWorker = {
+			port: { postMessage: vi.fn() },
+			postMessage: vi.fn()
+		} as unknown as TestingSharedWorker
+	})
 
-				expect(mockWorker.port.postMessage).toHaveBeenLastCalledWith({
-					type: UpstreamWorkerMessageType.Transition,
-					data: {
-						action: 'shift_foo_bar',
-						impact: TransitionImpact.LocalOnly
-					}
-				})
-				expect(mockWorker.postMessage).not.toBeCalled()
+	describe('message posting via .port.postMessage()', () => {
+		it('does not send any message on construction', () => {
+			new BrowserLocalFirst(mockWorker)
+			expect(mockWorker.port.postMessage).not.toHaveBeenCalled()
+			expect(mockWorker.postMessage).not.toHaveBeenCalled()
+		})
+		it('sends transitions', () => {
+			const syncEngine = new BrowserLocalFirst<TestingTransition>(mockWorker)
+			syncEngine.transition({
+				action: 'shift_foo_bar',
+				impact: TransitionImpact.LocalOnly
 			})
+
+			expect(mockWorker.port.postMessage).toHaveBeenLastCalledWith({
+				type: UpstreamWorkerMessageType.Transition,
+				data: {
+					action: 'shift_foo_bar',
+					impact: TransitionImpact.LocalOnly
+				}
+			} satisfies UpstreamWorkerMessage<TestingTransition>)
+			expect(mockWorker.postMessage).not.toBeCalled()
+		})
+		it('sends Close on dispose', () => {
+			const syncEngine = new BrowserLocalFirst<TestingTransition>(mockWorker)
+			syncEngine[Symbol.dispose]()
+			expect(mockWorker.port.postMessage).toHaveBeenLastCalledWith({
+				type: UpstreamWorkerMessageType.Close
+			} satisfies UpstreamWorkerMessage<TestingTransition>)
+			expect(mockWorker.postMessage).not.toBeCalled()
 		})
 	})
 })
