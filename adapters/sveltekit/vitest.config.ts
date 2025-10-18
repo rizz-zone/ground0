5dcfa4700de50759d43c9b100586ca4caac882ba
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-10-18 10:20:33 +0100

    just start exporting something from the new adapter

---
+++ adapters/sveltekit/vitest.config.ts
@@ -0,0 +1,20 @@
+import { configDefaults, defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+	test: {
+		exclude: [...configDefaults.exclude, '**/*.config.ts'],
+		coverage: {
+			exclude: [
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				...configDefaults.coverage.exclude!,
+				'**/*.config.ts',
+				'**/testing/**'
+			],
+			reporter: ['lcov', 'text']
+		},
+		globals: true,
+		environment: 'jsdom'
+	},
+	plugins: [tsconfigPaths()]
+})
