8adf0cba4cd91752d9f5b1d069615f82fdd41a36
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-10-26 09:56:18 +0000

    Discourage not providing type params

--- sample_integrations/counter/sveltekit/src/routes/+page.svelte
+++ sample_integrations/counter/sveltekit/src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import { createSyncEngine } from '@ground0/adapter-svelte'
+	import { createSyncEngine } from '@ground0/adapter-svelte'
 
-    createSyncEngine(new URL('http://localhost:5173/c.js'))
+	const engine = createSyncEngine(new URL('http://localhost:5173/c.js'))
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
