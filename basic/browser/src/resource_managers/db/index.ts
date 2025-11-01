cb3ea8e08c16da6c2a23e438dea6e5a7711b8378
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-11-01 11:36:37 +0000

    start handling messages

--- basic/browser/src/resource_managers/db/index.ts
+++ basic/browser/src/resource_managers/db/index.ts
@@ -28,16 +28,125 @@ type SomeAsyncSuccessfulWorkerResultHandler =
 
 export class DbThinClient {
 	private port?: MessagePort
-	private syncResources: (modifications: Partial<ResourceBundle>) => void
-	private dbName: string
-	private migrations: GeneratedMigrationSchema
+	private syncDbResource: (newDb: ResourceBundle['db']) => void
+	private migrations
+	private dbName
 
-	constructor()
+	private status = DbResourceStatus.Disconnected
+
+	constructor({
+		syncResources,
+		migrations,
+		dbName
+	}: {
+		syncResources: (modifications: Partial<ResourceBundle>) => void
+		migrations: GeneratedMigrationSchema
+		dbName: string
+	}) {
+		this.syncDbResource = (newDb) => {
+			this.status = newDb.status
+			syncResources({ db: newDb })
+		}
+		this.migrations = migrations
+		this.dbName = dbName
+	}
+
+	// The sync engine has 40 seconds to connect to the db before we assume
+	// it will never succeed.
+	private neverConnectingTimeout = setTimeout(
+		() => this.syncDbResource({ status: DbResourceStatus.NeverConnecting }),
+		40 * 1000
+	)
 
 	public newPort(port: MessagePort) {
-		if (!port) {
-		}
+		// Cleanup and replacement
+		this.port?.close()
 		this.port = port
+
+		// New port handlers
+		this.port.onmessage = ({
+			data: message
+		}: MessageEvent<DownstreamDbWorkerMessage>) => {
+			// Shouldn't be hit because the db worker should use a lock, but
+			// just in case
+			if (this.port !== port) return
+
+			switch (message.type) {
+				case DownstreamDbWorkerMessageType.NotConnecting:
+					// We only set this.port to undefined because there might
+					// be another tab that opens and can successfully provide
+					// a database connection, so we rely solely on
+					// neverConnectingTimeout for controlling when to mark as
+					// never connecting.
+					this.port = undefined
+					break
+				case DownstreamDbWorkerMessageType.Ready: {
+					const opLocked<T> = (callback: Parameters<(typeof navigator.locks.request)>[2]) => navigator.locks.request(`ground0::dbop_${this.dbName}`, callback)
+
+					try {
+						// Migrate and pass
+						const db = drizzle(
+							async (...input) => {
+								return (await navigator.locks.request(
+									`ground0::dbop_${dbName}`,
+									() => {
+										dbWorker.postMessage({
+											type: UpstreamDbWorkerMessageType.ExecOne,
+											params: input
+										} satisfies UpstreamDbWorkerMessage)
+										return lockedThenable
+									}
+								)) as (DownstreamDbWorkerMessage & {
+									type: DownstreamDbWorkerMessageType.SingleSuccessfulExecResult
+								})['result']
+							},
+							async (...input) => {
+								return (await navigator.locks.request(
+									`ground0::dbop_${dbName}`,
+									() => {
+										dbWorker.postMessage({
+											type: UpstreamDbWorkerMessageType.ExecBatch,
+											params: input
+										} satisfies UpstreamDbWorkerMessage)
+										return lockedThenable
+									}
+								)) as (DownstreamDbWorkerMessage & {
+									type: DownstreamDbWorkerMessageType.BatchSuccessfulExecResult
+								})['result']
+							}
+						)
+	
+						migrate(db, migrations).then(
+							() => {
+								brandedLog(console.debug, 'db is now migrated, syncing resources')
+								syncResources({
+									db: {
+										status: DbResourceStatus.ConnectedAndMigrated,
+										instance: db
+									}
+								})
+							},
+							(e) => {
+								brandedLog(
+									console.error,
+									'A migration error occurred while wrapping the nested worker:',
+									e
+								)
+								signalNeverConnecting()
+							}
+						)
+					} catch (e) {
+						brandedLog(
+							console.error,
+							'An error occurred while wrapping the nested worker:',
+							e
+						)
+						signalNeverConnecting()
+					}
+					break
+				}
+			}
+		}
 	}
 }
 
