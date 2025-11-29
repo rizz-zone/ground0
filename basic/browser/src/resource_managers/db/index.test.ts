f720259c708878f5dc2fe9f1c7c50eec8aee11d5
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-11-29 16:47:17 +0000

    write like two more tests

--- basic/browser/src/resource_managers/db/index.test.ts
+++ basic/browser/src/resource_managers/db/index.test.ts
@@ -6,6 +6,9 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 
 const { DbThinClient } = await import('./index')
 
+const TIMEOUT_NUMBER = 23443
+const setTimeoutMock = vi.spyOn(globalThis, 'setTimeout')
+
 const syncResources = vi.fn()
 const inputs = {
 	syncResources,
@@ -13,39 +16,70 @@ const inputs = {
 	dbName: 'kevin'
 } as const as ConstructorParameters<typeof DbThinClient>[0]
 
-beforeEach(vi.clearAllMocks)
+beforeEach(() => {
+	vi.clearAllMocks()
+	setTimeoutMock.mockImplementation(
+		() => TIMEOUT_NUMBER as unknown as ReturnType<typeof setTimeout>
+	)
+})
 
-describe('constructor', () => {
-	it('sets this.migrations to provided migrations', () => {
-		const client = new DbThinClient(inputs)
-		// @ts-expect-error For this test, we want to access the private member
-		expect(client.migrations).toBe(migrations)
-	})
-	it('sets this.dbName to provided dbName', () => {
-		const client = new DbThinClient(inputs)
-		// @ts-expect-error For this test, we want to access the private member
-		expect(client.dbName).toBe(inputs.dbName)
-	})
-	it('sets this.syncDbResource to method that calls syncResources and sets this.status', () => {
-		const client = new DbThinClient(inputs)
-		// @ts-expect-error We are testing the private method
-		const { syncDbResource } = client
-		expect(syncResources).not.toHaveBeenCalled()
-		// @ts-expect-error We are testing with the private value
-		expect(client.status).toBe(DbResourceStatus.Disconnected)
-		for (const [index, syncValue] of Object.entries([
-			{
-				status: DbResourceStatus.ConnectedAndMigrated,
-				instance: {} as LocalDatabase
-			},
-			{ status: DbResourceStatus.NeverConnecting }
-		] satisfies ResourceBundle['db'][])) {
-			syncDbResource(syncValue)
-			expect(syncResources).toHaveBeenNthCalledWith(Number(index) + 1, {
-				db: syncValue
-			} satisfies Partial<ResourceBundle>)
+describe('init', () => {
+	describe('constructor', () => {
+		it('sets migrations to provided migrations', () => {
+			const client = new DbThinClient(inputs)
+			// @ts-expect-error For this test, we want to access the private member
+			expect(client.migrations).toBe(migrations)
+		})
+		it('sets dbName to provided dbName', () => {
+			const client = new DbThinClient(inputs)
+			// @ts-expect-error For this test, we want to access the private member
+			expect(client.dbName).toBe(inputs.dbName)
+		})
+		it('sets syncDbResource to method that calls syncResources and sets local status copy', () => {
+			const client = new DbThinClient(inputs)
+			// @ts-expect-error We are testing the private method
+			const { syncDbResource } = client
+			expect(syncResources).not.toHaveBeenCalled()
 			// @ts-expect-error We are testing with the private value
-			expect(client.status).toBe(syncValue.status)
-		}
+			expect(client.status).toBe(DbResourceStatus.Disconnected)
+			for (const [index, syncValue] of Object.entries([
+				{
+					status: DbResourceStatus.ConnectedAndMigrated,
+					instance: {} as LocalDatabase
+				},
+				{ status: DbResourceStatus.NeverConnecting }
+			] satisfies ResourceBundle['db'][])) {
+				syncDbResource(syncValue)
+				expect(syncResources).toHaveBeenNthCalledWith(Number(index) + 1, {
+					db: syncValue
+				} satisfies Partial<ResourceBundle>)
+				// @ts-expect-error We are testing the private value
+				expect(client.status).toBe(syncValue.status)
+			}
+		})
+	})
+	describe('neverConnectingTimeout', () => {
+		it('is created with setTimeout', () => {
+			expect(setTimeoutMock).not.toHaveBeenCalled()
+			// @ts-expect-error We are testing the private value
+			const { neverConnectingTimeout } = new DbThinClient(inputs)
+			expect(setTimeoutMock).toHaveBeenCalledOnce()
+			expect(neverConnectingTimeout).toBe(TIMEOUT_NUMBER)
+		})
+		it('sets db status to DbResourceStatus.NeverConnecting if complete', ({
+			skip
+		}) => {
+			const client = new DbThinClient(inputs)
+			const syncDbResource = vi
+				.spyOn(client, 'syncDbResource' as keyof typeof client)
+				.mockImplementation(() => {})
+			const callback = setTimeoutMock.mock.lastCall?.[0]
+			if (!callback) return skip()
+			expect(syncDbResource).not.toHaveBeenCalled()
+			callback()
+			expect(syncDbResource).toHaveBeenCalledExactlyOnceWith({
+				status: DbResourceStatus.NeverConnecting
+			} satisfies ResourceBundle['db'])
+		})
 	})
 })
